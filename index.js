fc5c80e8b7e9e642d3340edd3526606cd2dd99ae
rhexa <akun.kripitit@gmail.com>
2022-02-10 19:39:04 +0200

    added portfolios section

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ app.use('/src', express.static('src'))
 
 app.get('/', (req,res)=>{
     const skillList = require('./src/json/skills.json')
-    res.render('index', {skillList});
+    const projects = [
+        {
+            name: "Project 1",
+            image: "src/img/showcase1.jpg",
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At rerum modi iure quo facere? Consequatur officiis eius eos itaque molestiae corrupti illo molestias magnam? Eaque nulla beatae deleniti adipisci placeat?",
+            tags: ["node JS", "Express JS", "EJS"],
+            github: "#",
+            url: "#"
+        },
+        {
+            name: "Project 2",
+            image: "src/img/showcase1.jpg",
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At rerum modi iure quo facere? Consequatur officiis eius eos itaque molestiae corrupti illo molestias magnam? Eaque nulla beatae deleniti adipisci placeat?",
+            tags: ["node JS", "Express JS", "Rest API"],
+            github: "#",
+            url: "#"
+        },
+    ]
+    res.render('index', {skillList, projects});
 })
 
 app.listen(port, ()=>console.log(`listening on port ${port}`));
