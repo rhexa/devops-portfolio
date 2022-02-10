356ca6724e0d11f34423a25384b54786029eb317
rhexa <akun.kripitit@gmail.com>
2022-02-11 00:16:50 +0200

    added form handler

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 3000;
+const mail = require('./modules/mail');
 
 app.set('view engine', 'ejs');
 
-// app.use('/src', express.static(path.join(__dirname, 'src')))
+app.use(express.urlencoded({extended: true}))
 app.use('/src', express.static('src'))
 
 app.get('/', (req,res)=>{
-    res.render('index', require('./src/json/main.json'));
+    const formSubmitted = req.query.formSubmitted
+    const datas = require('./src/json/main.json')
+    res.render('index', {...datas, formSubmitted});
+})
+
+app.post('/send', (req,res)=>{
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+    const output = `Submitted <br>${name}<br>${email}<br>${message}`
+    console.log(req.body);
+
+    const tmp = {
+        from: `"${req.body.name}" <${req.body.email}>`,
+        subject: "Email from rhexa portfolio website",
+        text: req.body.message
+    }
+    mail.send(tmp)
+    res.redirect('/?formSubmitted=true')
 })
 
 app.listen(port, ()=>console.log(`listening on port ${port}`));
