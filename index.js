55a6673fb1163a8fb5c29427a71930c34a702f86
rhexa <akun.kripitit@gmail.com>
2022-02-10 20:00:16 +0200

    changed main.json as the source

--- index.js
+++ index.js
@@ -8,26 +8,7 @@ app.set('view engine', 'ejs');
 app.use('/src', express.static('src'))
 
 app.get('/', (req,res)=>{
-    const skillList = require('./src/json/skills.json')
-    const projects = [
-        {
-            name: "Project 1",
-            image: "src/img/showcase1.jpg",
-            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At rerum modi iure quo facere? Consequatur officiis eius eos itaque molestiae corrupti illo molestias magnam? Eaque nulla beatae deleniti adipisci placeat?",
-            tags: ["node JS", "Express JS", "EJS"],
-            github: "#",
-            url: "#"
-        },
-        {
-            name: "Project 2",
-            image: "src/img/showcase1.jpg",
-            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At rerum modi iure quo facere? Consequatur officiis eius eos itaque molestiae corrupti illo molestias magnam? Eaque nulla beatae deleniti adipisci placeat?",
-            tags: ["node JS", "Express JS", "Rest API"],
-            github: "#",
-            url: "#"
-        },
-    ]
-    res.render('index', {skillList, projects});
+    res.render('index', require('./src/json/main.json'));
 })
 
 app.listen(port, ()=>console.log(`listening on port ${port}`));
