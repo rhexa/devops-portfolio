cf0144f83f0b9d1712b1d2fb0fda03c87d526372
rhexa <akun.kripitit@gmail.com>
2022-02-10 15:03:07 +0200

    init npm project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+// app.use('/src', express.static(path.join(__dirname, 'src')))
+app.use('/src', express.static('src'))
+
+app.get('/', (req,res)=>{
+    res.render('index');
+})
+
+app.listen(port, ()=>console.log(`listening on port ${port}`));
