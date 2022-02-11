50e2c7aa70a8191ed8a02952e6e6400e680d3359
rhexa <akun.kripitit@gmail.com>
2022-02-11 14:43:20 +0200

    updated port from env variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mail = require('./modules/mail');
 
 app.set('view engine', 'ejs');
