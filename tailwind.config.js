26f9da7585b6b1e518dc7eb50158c308e9e70f71
rhexa <akun.kripitit@gmail.com>
2022-02-12 16:09:58 +0200

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["test.html"],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins']
+      },
+      colors: {
+        'turqoise-0': '#E7FDF7',
+        'turqoise-1': '#A0F8DE',
+        'turqoise-2': '#5CEFC3',
+        'turqoise-3': '#32EFB6',
+        'turqoise-4': '#478170',
+        'turqoise-5': '#3F7364',
+      }
+    },
+  },
+  plugins: [],
+}
